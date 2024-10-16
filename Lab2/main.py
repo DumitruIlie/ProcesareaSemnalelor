e73e2418d721c70a6b57d29da932b45152ce16c0
DumitruIlie <iliemity@yahoo.com>
2024-10-16 21:45:04 +0300

    Am uitat scala logaritmica a erorii

--- Lab2/main.py
+++ Lab2/main.py
@@ -111,10 +111,14 @@ def ex_8():
 	#  f=sinusoidal(1, 0.5/np.pi, 0)
 	f=np.sin
 	p=lambda x : (x-7*x*x*x/60)/(1+x*x/20)
-	fig, axs=plt.subplots(3)
+	fig, axs=plt.subplots(5)
+	for ax in axs[3:]:
+		ax.set_yscale("log")
 	axs[0].plot(x, i(x))
 	axs[1].plot(x, p(x))
 	axs[2].plot(x, f(x))
+	axs[3].plot(x, np.abs(p(x)-f(x)))
+	axs[4].plot(x, np.abs(i(x)-f(x)))
 	plt.savefig("Plot_ex_8.pdf")
 	plt.cla()
 
